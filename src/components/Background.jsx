97c8945fc7de89cf4daf5131aa000120758a41e8
Samuel Abraham <sam.abraham6689@gmail.com>
2025-07-20 15:40:59 -0400

    creating background

---
+++ src/components/Background.jsx
@@ -0,0 +1,21 @@
+import "./Background.css"
+
+const background = () => {
+   return (
+   <div id="white_dust_background">
+   <img src="/white_dust.png"/>
+   <img src="/white_dust.png"/>
+   
+      
+   </div>
+   
+   
+
+
+
+   )
+}
+
+export default background;
+
+
