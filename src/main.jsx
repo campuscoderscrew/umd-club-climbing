c08946f16ea8ab4a19fc9176536c4be6f0398c00
Samuel Abraham <sam.abraham6689@gmail.com>
2025-07-22 14:39:47 -0400

    added video component

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import Background from './components/background.jsx'
 import Text from './components/text.jsx'
+import Video from './components/Video.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Text/>
+    <Video/>
     <Background/>
   </StrictMode>,
 )
