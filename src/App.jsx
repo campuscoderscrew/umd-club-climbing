2090b1fd35355f5a5a342f60b39b465a56f6bfc2
Samuel Abraham <sam.abraham6689@gmail.com>
2025-07-23 19:20:07 -0400

    fixed issue with main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Background from './components/background.jsx'
+import Text from './components/text.jsx'
+import Video from './components/Video.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+        <Text/>
+        <Video/>
+        <Background/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Background from './components/background.jsx'
-import Text from './components/text.jsx'
-import Video from './components/Video.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Text/>
-    <Video/>
-    <Background/>
+   <App/>
   </StrictMode>,
 )
