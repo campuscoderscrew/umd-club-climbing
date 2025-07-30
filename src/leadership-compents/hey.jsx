e4545333fb272c976d7787478d2f3bc58d6db419
Lillianne Davis <lilliannedavis@macbookpro.mynetworksettings.com>
2025-07-30 18:10:41 -0400

    hi

---
+++ src/leadership-compents/hey.jsx
@@ -0,0 +1,33 @@
+import './box.css'
+import './heading.css'
+import './imgs.css'
+import climbingbigHold from './assets/big_hold.png'; 
+import climbingsmallHold from './assets/small_hold.png'; 
+import React from "react";
+
+export const leadershipheading = ({ src, title }) => {
+ return (
+     <div>
+     <div className="pres">
+      <h1>Our Board Members</h1>
+    </div>
+    <div className="pres">
+      <h1>President</h1>
+    </div>
+    <div className="box">
+      <div style={boxStyle}></div>
+     </div>
+     <div className="firstname">
+      <h1>SKYLAR</h1>
+     </div>
+     <div className="lastname">
+      <h1>STRICKLER</h1>
+      </div>
+    <div style={hobbiesHeadingStyle}>
+      <h1>Favorite climb: Ones with rocks</h1>
+      </div>
+      <img src={climbingbigHold} alt="Climbing Hold" style={imageStyle} />
+      <img src={climbingsmallHold} alt="Climbing Hold" style={imagesmallStyle} />
+  </div>
+ );
+};
