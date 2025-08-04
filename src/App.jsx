7af2a02ac1fff45a6fdfceeebed90f25d9e8f914
Parsa Teymourlouei <parsa.t@Parsas-MacBook-Pro.local>
2025-08-04 15:23:32 -0400

    Cleaned up navbar, linked the tabs, aand changed fonts for Wall waiver section

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,46 @@ import Text from './components/Text';
 import Location from './components/Location';
 import JoinTheJourney from './components/JoinTheJourney';
 import WallWaiver from './components/WallWaiver';
-import NavBar from './components/Navbar';
-
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <>
-      <NavBar></NavBar>
-      <LandingPage />
-      <Text
-        highlight="Our Mission"
-        title="Climbing for all, Built with Purpose"
-        description="We aim to grow the climbing community by offering fun experiences where members can stay active, connect with others, and build their skills."
-         marginTop="300px"
-      />
-       <div class="center-box"></div>
-       <Text
-        highlight="Services"
-        title="Your Climbing Experience"
-        description="Open to everyone of varying skill level"
-         marginTop="100px"
-      />
-      <Location></Location>
-      <JoinTheJourney></JoinTheJourney>
-      <WallWaiver></WallWaiver>
+      <NavBar />
+
+      <section id="home">
+        <LandingPage />
+      </section>
+
+      <section id="about">
+        <Text
+          highlight="Our Mission"
+          title="Climbing for all, Built with Purpose"
+          description="We aim to grow the climbing community by offering fun experiences where members can stay active, connect with others, and build their skills."
+          marginTop="300px"
+        />
+      </section>
+
+      <div className="center-box"></div>
+
+      <section id="services">
+        <Text
+          highlight="Services"
+          title="Your Climbing Experience"
+          description="Open to everyone of varying skill level"
+          marginTop="100px"
+        />
+      </section>
+
+      <section id="times">
+        <Location />
+      </section>
+
+      <section id="join">
+        <JoinTheJourney />
+      </section>
+
+      <WallWaiver />
     </>
   );
 }
