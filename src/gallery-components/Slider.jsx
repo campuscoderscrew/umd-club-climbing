a29ee4a4804384be2bbf5181a9e54aac81e015a7
Susanna Zhang <ray@mac.mynetworksettings.com>
2025-08-06 19:38:16 -0400

    added Slider.css and Slider.jsx

---
+++ src/gallery-components/Slider.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import "./Slider.css";
+
+
+function Slider({ id, images, direction }) {
+   const [slides, setSlides] = useState([]);
+   const [slideCount, setSlideCount] = useState(0);
+   const [slideWidth, setSlideWidth] = useState(0);
+
+
+ useEffect(() => {
+   setSlides([...images, ...images]);
+   setSlideCount(images.length);
+ }, [images]);
+
+
+  return (
+   <div className="slider" id={id}>
+     <div
+       className={`slider-track ${
+         direction === "right" ? "scrollRight" : "scrollLeft"
+       }`}
+     >
+       {slides.map((src, index) => (
+         <div className="slide" key={index}>
+           <img src={src} alt={`Slide ${index + 1}`} />
+         </div>
+       ))}
+     </div>
+   </div>
+ );
+}
+
+
+export default Slider;
+
+
