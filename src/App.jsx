4ba305c38810369847f0285356c08146fba2110a
Samuel Abraham <sam.abraham6689@gmail.com>
2025-08-09 12:40:49 -0400

    current main on git hub

--- src/App.jsx
+++ src/App.jsx
@@ -47,35 +47,3 @@ function App() {
 }
 
 export default App;
-
-import './App.css';
-import LandingPage from './components/LandingPage';
-import Text from './components/Text';
-import Location from './components/Location';
-import JoinTheJourney from './components/JoinTheJourney';
-
-
-function App() {
-  return (
-    <>
-      <LandingPage />
-      <Text
-        highlight="Our Mission"
-        title="Climbing for all, Built with Purpose"
-        description="We aim to grow the climbing community by offering fun experiences where members can stay active, connect with others, and build their skills."
-         marginTop="300px"
-      />
-       <div class="center-box"></div>
-       <Text
-        highlight="Services"
-        title="Your Climbing Experience"
-        description="Open to everyone of varying skill level"
-         marginTop="100px"
-      />
-      <Location></Location>
-      <JoinTheJourney></JoinTheJourney>
-    </>
-  );
-}
-
-export default App;
