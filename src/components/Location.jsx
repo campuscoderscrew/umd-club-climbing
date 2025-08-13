bc2725c4cd65df65a0828dc83b810e748ecd4951
imustefa <imustefa@terpmail.umd.edu>
2025-08-13 15:41:48 -0400

    map marker, button, and responsivness adjusted

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { GoogleMap, LoadScript} from "@react-google-maps/api";
 import iphoneFrame from "../assets/iphone15.png";
+import pin from "../assets/Pin.png";
 import "./Location.css";
 
 const apiKey = import.meta.env.VITE_KEY; {/*.env file must be in root directory for api key import to work*/}
+const mapId = import.meta.env.VITE_MAP_ID
 const center = { lat: 38.9936, lng: -76.9452 };
+const libraries = ["marker"];
 
 export default function Location() {
   return (
@@ -21,20 +24,27 @@ export default function Location() {
               target="_blank"
               href="https://www.google.com/maps/place/Geary+F.+Eppley+Recreation+Center,+4128+Valley+Dr,+College+Park,+MD+20742/@38.9935511,-76.9496274,16.31z"
             >
-            <button type="button">Open Map</button>
+            <button type="button"><img src={pin}/><span>Open Map</span></button>
             </a>
           </div>
 
           <div className="phone-wrapper">
             <div className="map-mask">
-              <LoadScript googleMapsApiKey={apiKey}>
+              <LoadScript googleMapsApiKey={apiKey} libraries={libraries}>
                 <GoogleMap
                   mapContainerStyle={{ width: "100%", height: "98%" }}
                   center={center}
-                  zoom={16}
-                  options={{ disableDefaultUI: true }}
-                >
-                  <gmp-advanced-marker position={center} ></gmp-advanced-marker> 
+                  zoom={17}
+                  options={{ disableDefaultUI: true , mapId}}
+                  onLoad={(map)=> {
+                    const { AdvancedMarkerElement } = google.maps.marker;
+                    new AdvancedMarkerElement({
+                    map,
+                    position: center,
+                  title: "Eppley Recreation Center",
+                          });
+                        }}
+                  >
                   
                   {/*changed to a non depracted element*/}
                 </GoogleMap>
