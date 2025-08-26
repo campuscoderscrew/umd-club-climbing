44777f3b67a7217bd76e0ee1dc7400c4d6135795
Samuel Abraham <sam.abraham6689@gmail.com>
2025-08-25 22:17:33 -0400

    added ellipse background to icons

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -2,28 +2,34 @@ import './Icon.css';
 import Mountain from '../assets/mountain_icon.svg';
 import Globe from '../assets/globe_icon.svg';
 import Carabiner from '../assets/carabiner_icon.svg';
-  
+import Ellipse from '../assets/ellipse.svg';
 
-const Icon = ({ marginTop = '100px'}) => {
+const Icon = ({ marginTop = '100px' }) => {
   return (
-
     <div className="icon-container" style={{ marginTop }}>
       <div className='mountain'>
-      <img src = {Mountain}/>
-      <h3>Free Climbing Lessons</h3>
+        <div className="icon-with-ellipse">
+          <img src={Ellipse} alt="Ellipse background" />
+          <img src={Mountain} alt="Mountain icon" />
+        </div>
+        <h3>Free Climbing Lessons</h3>
       </div>
 
       <div className='carabiner'>
-      <img src = {Carabiner}/>
-      <h3>Gear Renting</h3>
+        <div className="icon-with-ellipse">
+          <img src={Ellipse} alt="Ellipse background" />
+          <img src={Carabiner} alt="Carabiner icon" />
+        </div>
+        <h3>Gear Renting</h3>
       </div>
 
       <div className='globe'>
-      <img src = {Globe}/>
-      <h3>Meeting New People</h3>
+        <div className="icon-with-ellipse">
+          <img src={Ellipse} alt="Ellipse background" />
+          <img src={Globe} alt="Globe icon" />
+        </div>
+        <h3>Meeting New People</h3>
       </div>
-      
-    
     </div>
   );
 };
